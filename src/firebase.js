43785916d9fd043221b3afa7addbd8b9d1da2b20
Vineeth pawar R <vineethpawar99@gmail.com>
2021-05-18 20:35:42 +0530

    finishing Post component with firebase

--- src/firebase.js
+++ src/firebase.js
@@ -12,7 +12,6 @@ const firebaseConfig = {
 
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db=firebaseApp.firestore();
-
   const auth=firebase.auth();
 
   export {db,auth};
